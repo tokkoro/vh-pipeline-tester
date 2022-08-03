149972969059a21e628d831860b0ccd174942aa4
Tomi Kokkonen <tomi@valohai.com>
2022-08-03 14:59:44 +0300

    add file step

---
+++ io_files.py
@@ -0,0 +1,30 @@
+import json
+import valohai
+
+
+def log_metadata(md):
+    print(json.dumps(md))
+
+
+inputs = {
+    "input_1": "",
+    "input_2": "",
+}
+
+valohai.prepare(
+    step='step-io',
+    image='python',
+    default_inputs=inputs
+)
+
+param_1 = valohai.parameters('param_1').value
+
+for data_path in valohai.inputs("input_1").paths():
+    with open(data_path) as f:
+        data = json.load(f)
+        print(f"my file contains {data}")
+
+out_path = valohai.outputs('output_1').path('output_1.json')
+
+with open(out_path, "w") as f:
+    f.write(json.dumps({"dataa": "on", "kaljaa": "ei"}))
