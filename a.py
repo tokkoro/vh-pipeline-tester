55709430cea4b74f97467448e2cb2b575aaa72af
Tomi Kokkonen <tomi@valohai.com>
2022-09-30 10:44:21 +0300

    add param-to-param config

--- a.py
+++ a.py
@@ -6,14 +6,6 @@ def log_metadata(md):
     print(json.dumps(md))
 
 
-valohai.prepare(
-    step='step-a',
-    image='python',
-    default_parameters={
-        'exec_id': 1
-    }
-)
-
 exec_id = valohai.parameters('exec_id').value
 log_metadata({"id": exec_id, "next_ids": [13, 15, 17]})
 log_metadata({"last": True, "percent": 1})
