52992e1571b6aab9c36503dfc91b2db8116de143
Tomi Kokkonen <tomi@valohai.com>
2023-02-27 14:12:01 +0200

    Pipeline-test: flag edge from execution to task

--- a.py
+++ a.py
@@ -9,3 +9,5 @@ def log_metadata(md):
 exec_id = valohai.parameters('exec_id').value
 log_metadata({"id": exec_id, "next_ids": [13, 15, 17]})
 log_metadata({"last": True, "percent": 1})
+
+log_metadata({"publish_results": "False"})
