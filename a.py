e17445ebb76602631b95351dcf9becf7d48a38b6
Tomi Kokkonen <tomi@valohai.com>
2023-02-27 14:20:02 +0200

    Flag param: rename metadata from publish to flag

--- a.py
+++ a.py
@@ -10,4 +10,4 @@ exec_id = valohai.parameters('exec_id').value
 log_metadata({"id": exec_id, "next_ids": [13, 15, 17]})
 log_metadata({"last": True, "percent": 1})
 
-log_metadata({"publish_results": "False"})
+log_metadata({"flag_param": "False"})
